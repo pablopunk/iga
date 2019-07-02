da8777b9f9f97a5a3f949538616fb3d815cecf2c
pablopunk <pablovarela182@gmail.com>
2019-07-02 13:35:21 +0200

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -6,47 +6,46 @@ const getPort = require('get-port')
 
 const rootFolder = path.join(__dirname, 'example')
 
-test('Creates an index.js endpoint at /', async t => {
-  const port = await getPort()
-  const server = await m({ root: rootFolder, port, silent: true })
-  const res = await unfetch(`http://localhost:${port}/`)
-  const data = await res.text()
-  server.close()
-  t.is(data, 'hello from index.js')
-})
+function genericTest(endpoint, responseText) {
+  return async t => {
+    const port = await getPort()
+    const server = await m({ root: rootFolder, port, silent: true })
+    const res = await unfetch(`http://localhost:${port}${endpoint}`)
+    const data = await res.text()
+    server.close()
+    t.is(data, responseText)
+  }
+}
 
-test('Creates a named endpoint', async t => {
-  const port = await getPort()
-  const server = await m({ root: rootFolder, port, silent: true })
-  const res = await unfetch(`http://localhost:${port}/foo`)
-  const data = await res.text()
-  server.close()
-  t.is(data, 'hello from foo.js')
-})
+function genericStatusCodeTest(endpoint, expectedCode) {
+  return async t => {
+    const port = await getPort()
+    const server = await m({ root: rootFolder, port, silent: true })
+    const res = await unfetch(`http://localhost:${port}${endpoint}`)
+    const code = res.status
+    server.close()
+    t.is(code, expectedCode)
+  }
+}
 
-test('Returns 404 for empty endpoint', async t => {
-  const port = await getPort()
-  const server = await m({ root: rootFolder, port, silent: true })
-  const res = await unfetch(`http://localhost:${port}/idontexist`)
-  const code = res.status
-  server.close()
-  t.is(code, 404)
-})
+test(
+  'Creates an index.js endpoint at /',
+  genericTest('/', 'hello from index.js')
+)
 
-test('Supports nested folders with index.js support', async t => {
-  const port = await getPort()
-  const server = await m({ root: rootFolder, port, silent: true })
-  const res = await unfetch(`http://localhost:${port}/bar`)
-  const data = await res.text()
-  server.close()
-  t.is(data, 'hello from bar/index.js')
-})
+test('Creates a named endpoint', genericTest('/foo', 'hello from foo.js'))
 
-test('Supports typescript', async t => {
-  const port = await getPort()
-  const server = await m({ root: rootFolder, port, silent: true })
-  const res = await unfetch(`http://localhost:${port}/typescript`)
-  const data = await res.text()
-  server.close()
-  t.is(data, 'hello from typescript.ts')
-})
+test(
+  'Supports nested folders with index.js support',
+  genericTest('/bar', 'hello from bar/index.js')
+)
+
+test(
+  'Supports typescript',
+  genericTest('/typescript', 'hello from typescript.ts')
+)
+
+test(
+  'Returns 404 for empty endpoint',
+  genericStatusCodeTest('/idontexist', 404)
+)
